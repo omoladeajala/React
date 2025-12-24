cf13eeb09ad74c43470710de56f73aa3f54d5f8f
omoladeajala <omoladeajala2021@gmail.com>
2025-12-24 02:04:59 +0100

    fix: correct ReactDOM import and render structure in main.jsx

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,2 +1,13 @@
 import React from "react";
-import ReactDOM from "Reac";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Noodles</li>
+      <li>Chicken</li>
+    </ul>
+  </div>
+     document.getElementById("root"));
