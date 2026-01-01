3957d7fbb0c526e6bb620c4eb8448dc4827e0d00
omoladeajala <omoladeajala2021@gmail.com>
2026-01-01 19:06:30 +0100

    Update text color based on time

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let greetings = 
+const date = new Date();
+const currentTime = date.getHours();
 
-const hour = new Date().getHours();
-const currentTime = 
+let greetings;
 
+const customStyle = {
+  color: "",
+};
 
-if ( currentTime < 12) {
-  greetings = "Good Morning"
-} else if 
-    ( currentTime < 18 )
-  { greetings = "Good Afternoon"
+if (currentTime < 12) {
+  greetings = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greetings = "Good Afternoon";
+  customStyle.color = "green";
 } else {
-  greetings = "Good Evening"
+  greetings = "Good Night";
+  customStyle.color = "purple";
 }
+
 ReactDOM.render(
-  <h1 className="heading" style={{color: "blue"}}>{greetings}</h1>
-  ,document.getElementById("root"))
+  <h1 className="heading" style={{ color: "blue" }}>
+    {greetings}
+  </h1>,
+  document.getElementById("root")
+);
