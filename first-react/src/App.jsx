02c1cabccde59d37e57199c31ab156d72f824a11
omoladeajala <omoladeajala2021@gmail.com>
2026-01-01 18:48:14 +0100

    refactor: simplify App component by removing unused code and updating greeting logic

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+let greetings = 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const hour = new Date().getHours();
+const currentTime = 
+
+
+if ( currentTime < 12) {
+  greetings = "Good Morning"
+} else if 
+    ( currentTime < 18 )
+  { greetings = "Good Afternoon"
+} else {
+  greetings = "Good Evening"
 }
-
-export default App
+ReactDOM.render(
+  <h1 className="heading" style={{color: "blue"}}>{greetings}</h1>
+  ,document.getElementById("root"))
