6db3ceb04c47cf318d52b800464a49bbc736c123
omoladeajala <omoladeajala2021@gmail.com>
2026-01-03 10:16:56 +0100

    Updated the App.jsx component structure

--- second-react/src/App.jsx
+++ second-react/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Heading from "./Heading";
 
 function App() {
-  return;
-  <div>
-    <Heading />
-  </div>;
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
