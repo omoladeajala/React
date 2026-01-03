ec65863a2917b40733b588ce98b02c3cf34f48d4
omoladeajala <omoladeajala2021@gmail.com>
2026-01-03 10:48:51 +0100

    feat: import value from math module and render it

--- third-react/src/main.jsx
+++ third-react/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pi from "./math.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ul>
-    <li>1</li>
+    <li>{pi}</li>
     <li>2</li>
     <li>3</li>
   </ul>
